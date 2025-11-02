8ebf7b38564f7a9d4099a6bf645436e871655727
AlexReyes03 <20233tn160@utez.edu.mx>
2025-11-02 05:06:57 -0600

    [FIRST] Estructura del frontend

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client'
+import PrimeReact from 'primereact/api';
+
+import App from './App.jsx'
+
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// PrimeReact
+import 'primereact/resources/primereact.min.css';
+import 'primereact/resources/themes/lara-light-cyan/theme.css';
+import 'primeicons/primeicons.css';
+
+// Styles
+import './assets/css/global-styles.css';
+
+PrimeReact.ripple = true;
+PrimeReact.autoZIndex = true;
+PrimeReact.zIndex['overlay'] = 1200;
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
